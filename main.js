21b7fd663e2540b90d6e610430d206414a142687
Xiomara Arzola <102442089+Xiomiau@users.noreply.github.com>
2022-06-01 11:52:37 -0700

    Add files via upload
    Mediante un prompt el usuario debe escribir el dia de la semana, en número, y según el dia ingresado le da una frase.

---
+++ main.js
@@ -0,0 +1,27 @@
+let numeroDia = prompt("Ingresa el día de la semana, en número");
+switch(numeroDia){
+
+    case '1' : document.write("<h1>El día de hoy es <br> Lunes. <br><br> Órale a trabajar. </h1>");
+    break;
+
+    case '2' : document.write("<h1>El día de hoy es <br> Martes. <br><br> Nesflis day madafaca </h1>");
+    break;
+
+    case '3' : document.write("<h1>El día de hoy es <br> Miércoles.<br><br> Toca baño, hueles a cheto. </h1>");
+    break;
+
+    case '4' : document.write("<h1>El día de hoy es <br> Jueves. <br><br> Comer chuches de cena. </h1>");
+    break;
+
+    case '5' : document.write("<h1>El día de hoy es <br> Viernes. <br><br> Beviernes o que?. </h1>");
+    break;
+
+    case '6' : document.write("<h1>El día de hoy es <br> Sábado. <br><br> Toca limpiar la casa con música de Juanga </h1>");
+    break;
+
+    case '7' : document.write("<h1>El día de hoy es <br> Domingo. <br> <br>Ni jebus trabajó en domingo, duérmete. </h1>");
+    break;
+    default: document.write("<h1>Día desconocido. <br> <br> A mimir zzzz.</h1>");
+    break;
+    }
+
